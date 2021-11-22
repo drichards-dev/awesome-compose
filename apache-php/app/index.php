8d4e076364ef695a8f8463c674ab153dea046517
Duane Richards <duane.richards@gmail.com>
2021-11-21 21:51:49 -0600

    index.php edits

--- apache-php/app/index.php
+++ apache-php/app/index.php
@@ -1,3 +1,47 @@
-<?php 
-    echo '<h1>Hello World!</h1>'; 
-?> 
+<html>
+<head>
+    <style>
+        body {
+        font-family: verdana;  
+        background-color: lightgrey;       
+        }
+
+        h1 {
+        font-size: 25px;
+        color: white;
+        }
+        div {
+        
+        max-width: 400px;
+        align: center;  
+        background-color: lightslategrey; 
+        }
+        p {
+        margin: 1em;
+        font-size: 20px;
+        }
+    </style>
+
+    <title>Names</title>
+
+</head>
+
+<h1>Names and phrases</h1>
+    <div>
+    
+    <p>
+        </br>
+        <ul>
+            <li>incompleted articulation</li>
+            <li>unbridled frippery</li>
+            <li>acrylic porn</li>
+            <li>lorem ipsum dolor sit amet</li>
+        </ul>
+        </br>
+    </p>
+    
+    <p>See <a href='info.php'>info.php</a> for phpinfo ;)</p>
+    </br>
+    </div>
+
+</html>
